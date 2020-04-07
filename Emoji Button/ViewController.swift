2b751e1741683a05c34e16866598a8d1390cdb20
rabiiwoof <niecheeel@gmail.com>
2020-04-08 00:42:04 +0800

    Ver 1

--- Emoji Button/ViewController.swift
+++ Emoji Button/ViewController.swift
@@ -9,7 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    func alertMessage(title: String, message: String, button: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: button, style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
 
+    
+    @IBAction func angryMessage(sender: UIButton) {
+        alertMessage(title: "Angry", message: "I'm Angry", button: "Fuck off")
+    }
+    @IBAction func ewMessage(sender: UIButton) {
+        alertMessage(title: "Ewww", message: "I'm Ewww", button: "Fuck off")
+    }
+    @IBAction func frezzeMessage(sender: UIButton) {
+        alertMessage(title: "Frezzz", message: "I'm Frezzz", button: "Fuck off")
+    }
+    @IBAction func evilMessage(sender: UIButton) {
+        alertMessage(title: "Evil", message: "I'm Evil", button: "Fuck off")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
